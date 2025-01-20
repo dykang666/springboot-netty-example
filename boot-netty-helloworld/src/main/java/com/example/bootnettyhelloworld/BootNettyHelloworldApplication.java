46f21389b1685fbdc04240b07bdbf8eebccff574
dykang666 <kangdy@yuanian.com>
2025-01-20 23:42:30 +0800

    合并项目到netty-boot-helloworld

---
+++ boot-netty-helloworld/src/main/java/com/example/bootnettyhelloworld/BootNettyHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootnettyhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootNettyHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootNettyHelloworldApplication.class, args);
+    }
+
+}
---
+++ boot-netty-helloworld/src/test/java/com/example/bootnettyhelloworld/BootNettyHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootnettyhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootNettyHelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
